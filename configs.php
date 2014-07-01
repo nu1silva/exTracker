b68a865c574b4f1487404e09deaa4026fc6443f8
Nuwan Silva <nu1silva@gmail.com>
2014-07-01 21:00:08 +0530

    update with openshift credentials

--- configs.php
+++ configs.php
@@ -1,8 +1,8 @@
 <?php
-$mysql_hostname = "localhost";
-$mysql_user = "root";
-$mysql_password = "";
-$mysql_database = "time";
+$mysql_hostname = "127.6.63.2";
+$mysql_user = "adminnhVut3p";
+$mysql_password = "xXtma-A9byS1";
+$mysql_database = "extracker";
 
 
 $db = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_database);
